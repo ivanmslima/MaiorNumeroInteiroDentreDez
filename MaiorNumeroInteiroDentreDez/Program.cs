7bf0ec3d5252fbab556aba96b8d1be4b1174efeb
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-30 20:47:26 -0300

    Adicionar arquivos de projeto.

---
+++ MaiorNumeroInteiroDentreDez/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um número: ");
+            int numero = int.Parse(Console.ReadLine());
+            int maior = numero;
+            int cont = 1;
+
+            while(cont < 10)
+            {
+                Console.Write("Informe um número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if(numero > maior)
+                {
+                    maior = numero;
+                }
+                cont++;
+            }
+            Console.WriteLine("O maior numero: " + maior);
+
+
+        }
+    }
+}
